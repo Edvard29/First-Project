46b90edc5b053f20d2a99b20acb9f41148e329b0
Edvard29 <khachatryane11@gmail.com>
2024-12-28 16:05:30 +0400

    Delete app directory

--- app/src/main/java/FootballApiService.java
+++
@@ -1,8 +0,0 @@
-import retrofit2.Call;
-import retrofit2.http.GET;
-import retrofit2.http.Header;
-
-public interface FootballApiService {
-    @GET("fixtures")
-    Call<ApiResponse> getFixtures(@Header("X-RapidAPI-Key") String apiKey);
-}
